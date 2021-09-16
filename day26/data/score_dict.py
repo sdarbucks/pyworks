cd8a2a0b03989e1a75945fa98b18feccc0a10e4a
sdarbucks <kdh797979@naver.com>
2021-09-16 13:20:34 +0900

    day27 업로드

--- day26/data/score_dict.py
+++ day26/data/score_dict.py
@@ -1,29 +1,34 @@
 # 학생 5명 개인별 국어, 수학 점수 계산
 score = [
-    {'name': '이대한', 'korean': 90, 'math': 70},
-    {'name': '장민국', 'korean': 85, 'math': 76},
-    {'name': '오상식', 'korean': 80, 'math': 65},
-    {'name': '천선란', 'korean': 95, 'math': 85},
-    {'name': '김조엽', 'korean': 85, 'math': 70}
+    {'name': '이대한', 'korean': 90, 'math': 70, 'science': 65},
+    {'name': '장민국', 'korean': 85, 'math': 76, 'science': 75},
+    {'name': '오상식', 'korean': 80, 'math': 65, 'science': 85},
+    {'name': '천선란', 'korean': 95, 'math': 85, 'science': 55},
+    {'name': '김조엽', 'korean': 85, 'math': 70, 'science': 35}
 ]
 print(' 이름  총점 평균')
 for s in score:
-    sum_v = s['korean'] + s['math']
-    avg = sum_v / 2
-    print(s['name'], sum_v, avg)
+    sum_v = s['korean'] + s['math'] + s['science']
+    avg = sum_v / 3
+    print("%s %d %.1f" % (s['name'], sum_v, avg))
 
 # 과목별 국어 수학 점수 계산
 print('========== 과목별 국어 수학 점수 계산 ==========')
 sum_kor = 0
 sum_math = 0
+sum_sci = 0
 n = len(score)
 for s in score:
     sum_kor += s['korean']
     sum_math += s['math']
+    sum_sci += s['science']
 
 avg_kor = sum_kor / n  # 국어 평균
 avg_math = sum_math / n  # 수학 평균
+avg_sci = sum_sci / n  # 과학 평균
 print("국어 합계 : %d점" % sum_kor)
 print("수학 합계 : %d점" % sum_math)
+print("과학 합계 : %d점" % sum_sci)
 print("국어 평균 : %.1f점" % avg_kor)
 print("수학 평균 : %.1f점" % avg_math)
+print("과학 평균 : %.1f점" % avg_sci)
